791b381b2f09cd83b6f5cf2c06cbcdc9acbf80ff
Grant Lovell <g.lovell@vehikl.com>
2021-01-11 22:20:12 -0500

    Add feedback to the create script

--- racing-car/create.sh
+++ racing-car/create.sh
@@ -1,20 +1,28 @@
 #!/usr/bin/env bash
 
 # Get the project files
+echo 'Cloning GitHub Repo ...'
 git clone https://github.com/emilybache/Racing-Car-Katas.git ~/Code/katas/racing-car
+
+# Copy PHPStorm settings
+echo 'Copying PHPStorm settings ...'
+cp -R ./files/idea ~/Code/katas/racing-car/php/.idea
 
 cd ~/Scripts/katas/racing-car
 
+echo 'Launching docker file volume'
 docker-compose up -d files
 
+echo 'Loading files into volume ...'
 mutagen sync create --name=racing-car \
     --ignore=".idea" --ignore-vcs \
     --default-directory-mode=777 \
     --default-file-mode=666 \
     ~/Code/katas/racing-car/php docker://racing-car-files/project
 
+echo 'Launching app ...'
 docker-compose up -d app
 
+echo 'Installing composer dependencies ...'
 docker-compose exec app composer install
 
-cp -R ./files/idea ~/Code/katas/racing-car/php/.idea
