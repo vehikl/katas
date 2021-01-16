7b67f31dd9ddd2efa2a0d4ee418a8d0e5f4885ea
Grant Lovell <g.lovell@vehikl.com>
2021-01-16 10:00:57 -0500

    Use path variables

--- tennis/create.sh
+++ tennis/create.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
-# Get the project files
-git clone https://github.com/emilybache/Tennis-Refactoring-Kata.git ~/Code/katas/tennis
+SCRIPT_DIR=~/Scripts/katas/tennis
+SOURCE_DIR=~/Code/katas/tennis
 
-cd ~/Scripts/katas/tennis
+# Get the project files
+git clone https://github.com/emilybache/Tennis-Refactoring-Kata.git $SOURCE_DIR
+
+cd $SCRIPT_DIR
 
 docker-compose up -d files
 
@@ -11,12 +14,12 @@ mutagen sync create --name=tennis \
     --ignore=".idea" --ignore-vcs \
     --default-directory-mode=777 \
     --default-file-mode=666 \
-    ~/Code/katas/tennis/php docker://tennis-files/project
+    ${SOURCE_DIR}/php docker://tennis-files/project
 
 docker-compose up -d app
 
 docker-compose exec app composer install
 
-cp -R ~/Scripts/katas/tennis/files/idea ~/Code/katas/tennis/php/.idea
+cp -R ${SCRIPT_DIR}/files/idea ${SOURCE_DIR}/php/.idea
 
-pstorm ~/Code/katas/tennis/php || phpstorm ~/Code/katas/tennis/php
+pstorm ${SOURCE_DIR}/php || phpstorm ${SOURCE_DIR}/php
